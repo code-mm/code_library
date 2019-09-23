c163b663322eb617a317fb05753600934288341f
Markus Merklinger <markus.merklinger@code.berlin>
2019-09-23 23:14:29 +0200

    Adds book_id field in loan serializers

--- book_api/serializers.py
+++ book_api/serializers.py
@@ -38,15 +38,23 @@ class Book(serializers.ModelSerializer):
 class LoanReserved(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    book_id = serializers.SerializerMethodField()
+
+    def get_book_id(self, obj):
+      return models.BookCopies.objects.select_related('book').get(id=obj.book_copy.id).book.id
 
     class Meta:
         model = models.LoanReserved
-        fields = ('id', 'user', 'book_copy', 'duration')
+        fields = ('id', 'user', 'book_copy', 'duration', 'book_id')
 
 class Loan(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    book_id = serializers.SerializerMethodField()
+
+    def get_book_id(self, obj):
+      return models.BookCopies.objects.select_related('book').get(id=obj.book_copy.id).book.id
 
     class Meta:
         model = models.Loan
-        fields = ('id', 'user', 'book_copy', 'from_date', 'to_date')
+        fields = ('id', 'user', 'book_copy', 'from_date', 'to_date', 'book_id')
